aac4468c398efd1d9483c8504ab2d26d555ba67d
klin1344 <kevlin97@gmail.com>
2012-07-25 11:00:48 -0700

    Try to set default CPU frequencies, 192Mhz and 1.5 Ghz

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -235,6 +235,9 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	init_completion(&cpu_work->complete);
 #endif
 
+	policy->min = 192000;
+	policy->max = 1512000;
+
 	return 0;
 }
 
