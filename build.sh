0d8c7b5ed90fe3bcba9970cc373dee8d5133cd3c
klin1344 <kevlin97@gmail.com>
2012-08-02 14:09:47 -0700

    Bump to version 2.2

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 CROSS_COMPILE=/home/klin1344/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
 INITRAMFS_DIR=ramdisk.gz
 KERNEL_NAME=Fusion_Ultimate_Sense
-KERNEL_VNUMBER=2.1
+KERNEL_VNUMBER=2.2
 
 # DO NOT MODIFY BELOW THIS LINE
 CURRENT_DIR=`pwd`
